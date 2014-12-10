darksidelemm/open_radio_miniconf_2015

commit af4866f49b46374280b5e459d992a93f410f3867

interactive: Make libraries buildable
Some confusion with the types and scope is resolved by making the ring
buffer a cpp file (it's all C though), and adding a header for the
definition of pins.

Signed-off-by: Joel Stanley <joel@jms.id.au>

openradio_interactive/PSK.ino
@@ -21,8 +21,16 @@
     see <http://www.gnu.org/licenses/>.
 */
 
+#include <avr/pgmspace.h>
 
-static uint16_t bpsk_varicode[] = {
+#include "TimerOne.h"
+
+#include "ring_buffer.h"
+#include "pins.h"
+
+extern struct ring_buffer data_tx_buffer;
+
+PROGMEM static const uint16_t bpsk_varicode[] = {
 	0x0355, // 0x00 =>  1010101011 (NUL)
 	0x036d, // 0x01 =>  1011011011 (SOH)
 	0x02dd, // 0x02 =>  1011101101 (STX)
@@ -153,12 +161,10 @@ static uint16_t bpsk_varicode[] = {
 	0x02b7, // 0x7f =>  1110110101 (DEL)
 };
 
-
 // Phase Reversal routine. Currently this just toggles an IO line.
 void bpsk_phase_switch(){
     digitalToggleFast(MODULATION);
 }
-
 
 // Interrupt driven transmit routines
 

openradio_interactive/Ring_Buffer.ino
@@ -1,59 +1,0 @@
-/*
-	
-	Ring Buffer Functions.
-	Shamelessly ripped from the Arduino HardwareSerial libs.
-	
-	(Not) Copyright (C) 2014 Mark Jessop <vk5qi@rfhead.net>
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    For a full copy of the GNU General Public License, 
-    see <http://www.gnu.org/licenses/>.
-*/
-
-
-void store_char(unsigned char c, ring_buffer *buffer)
-{
-  int i = (unsigned int)(buffer->head + 1) % TX_BUFFER_SIZE;
-
-  // if we should be storing the received character into the location
-  // just before the tail (meaning that the head would advance to the
-  // current location of the tail), we're about to overflow the buffer
-  // and so we don't write the character or advance the head.
-  if (i != buffer->tail) {
-    buffer->buffer[buffer->head] = c;
-    buffer->head = i;
-  }
-}
-
-unsigned char read_char(ring_buffer *buffer)
-{
-  // if the head isn't ahead of the tail, we don't have any characters
-  if (buffer->head == buffer->tail) {
-    return 0;
-  } else {
-    unsigned char c = buffer->buffer[buffer->tail];
-    buffer->tail = (unsigned int)(buffer->tail + 1) % TX_BUFFER_SIZE;
-    return c;
-  }
-}
-
-unsigned int data_waiting(ring_buffer *buffer){
-    return (unsigned int)(TX_BUFFER_SIZE + buffer->head - buffer->tail) % TX_BUFFER_SIZE;
-}
-
-void store_string(char* string){
-	char c = *string++;
-	while (c != 0){
-		store_char(c,&data_tx_buffer);
-		c = *string++;
-	}
-}

openradio_interactive/openradio_interactive.ino
@@ -27,16 +27,8 @@
 #include "TimerOne.h"
 
 #include "settings.h"
-
-// Pin Definitions
-#define TX_RX_SWITCH    2
-#define MODULATION      3
-#define LED             13
-
-// Which Si5351 clock is used for what...
-#define RX_CLOCK    SI5351_CLK0
-#define TX_CLOCK    SI5351_CLK2
-#define SPARE_CLOCK    SI5351_CLK1
+#include "pins.h"
+#include "ring_buffer.h"
 
 // Default output frequency
 #define RX_FREQ 27000000
@@ -48,8 +40,9 @@
 // Other settings
 #define SERIAL_BAUD     57600
 
-// Fast pin toggling.
-#define digitalToggleFast(P) *portInputRegister(digitalPinToPort(P)) = digitalPinToBitMask(P)
+// Prototypes presnt in PSK.ino. Forward delcare them so ino can build
+void bpsk_start(int baud_rate);
+void bpsk_stop(void);
 
 Si5351 si5351;
 
@@ -59,15 +52,7 @@ int tx_state = 0;
 static struct settings settings;
 
 // Ring buffer for use with the interrupt-driven transmit buffer.
-#define TX_BUFFER_SIZE  64
-struct ring_buffer
-{
-  unsigned char buffer[TX_BUFFER_SIZE];
-  volatile unsigned int head;
-  volatile unsigned int tail;
-};
-
-ring_buffer data_tx_buffer = { { 0 }, 0, 0};
+struct ring_buffer data_tx_buffer = { { 0 }, 0, 0};
 
 /* Returns true if settings are valid */
 static bool read_settings(void)

openradio_interactive/pins.h
@@ -1,0 +1,19 @@
+#ifndef PINS_H
+#define PINS_H
+
+// Pin Definitions
+#define TX_RX_SWITCH    2
+#define MODULATION      3
+#define LED             13
+
+// Which Si5351 clock is used for what...
+#define RX_CLOCK    SI5351_CLK0
+#define TX_CLOCK    SI5351_CLK2
+#define SPARE_CLOCK    SI5351_CLK1
+
+
+// Fast pin toggling.
+#define digitalToggleFast(P) \
+    *portInputRegister(digitalPinToPort(P)) = digitalPinToBitMask(P)
+
+#endif /* PINS_H */

openradio_interactive/ring_buffer.cpp
@@ -1,0 +1,50 @@
+/*
+   Ring Buffer Functions.
+   Copied from the Arduino HardwareSerial libs.
+   Copyright (c) 2006 Nicholas Zambetti.  All right reserved.	
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   For a full copy of the GNU Lesser General Public License, 
+   see <http://www.gnu.org/licenses/>.
+*/
+
+#include "ring_buffer.h"
+
+void store_char(unsigned char c, struct ring_buffer *buffer)
+{
+  int i = (unsigned int)(buffer->head + 1) % TX_BUFFER_SIZE;
+
+  // if we should be storing the received character into the location
+  // just before the tail (meaning that the head would advance to the
+  // current location of the tail), we're about to overflow the buffer
+  // and so we don't write the character or advance the head.
+  if (i != buffer->tail) {
+    buffer->buffer[buffer->head] = c;
+    buffer->head = i;
+  }
+}
+
+unsigned char read_char(struct ring_buffer *buffer){
+  // if the head isn't ahead of the tail, we don't have any characters
+  if (buffer->head == buffer->tail) {
+    return 0;
+  } else {
+    unsigned char c = buffer->buffer[buffer->tail];
+    buffer->tail = (unsigned int)(buffer->tail + 1) % TX_BUFFER_SIZE;
+    return c;
+  }
+}
+
+unsigned int data_waiting(struct ring_buffer *buffer){
+    return (unsigned int)(TX_BUFFER_SIZE + buffer->head - buffer->tail) % TX_BUFFER_SIZE;
+}
+

openradio_interactive/ring_buffer.h
@@ -1,0 +1,17 @@
+#ifndef RING_BUFFER_H
+#define RING_BUFFER_H
+
+#define TX_BUFFER_SIZE  64
+
+struct ring_buffer {
+  unsigned char buffer[TX_BUFFER_SIZE];
+  volatile unsigned int head;
+  volatile unsigned int tail;
+};
+
+void store_char(unsigned char c, struct ring_buffer *buffer);
+unsigned char read_char(struct ring_buffer *buffer);
+unsigned int data_waiting(struct ring_buffer *buffer);
+void store_string(char* string);
+
+#endif /*RING_BUFFER_H*/

commit 7075fc425b3cac829fc7d7009764d04a71548106

interactive: Fix PSK ring buffer bug
I introduced a regression in af4866f49b4 (interactive: Make libraries
buildable) by putting bpsk_varicode into progmem without updating the
accessors.

As progmem access is broken on uin16_t arrays with the Arduino 1.0
toolchain, we will not put the varicode buffer in progmem.

Signed-off-by: Joel Stanley <joel@jms.id.au>

openradio_interactive/PSK.ino
@@ -30,7 +30,7 @@
 
 extern struct ring_buffer data_tx_buffer;
 
-PROGMEM static const uint16_t bpsk_varicode[] = {
+static const uint16_t bpsk_varicode[] = {
 	0x0355, // 0x00 =>  1010101011 (NUL)
 	0x036d, // 0x01 =>  1011011011 (SOH)
 	0x02dd, // 0x02 =>  1011101101 (STX)