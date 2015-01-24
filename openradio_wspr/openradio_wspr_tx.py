5270749769d84ba4d6bb4799bf8771c3bc441c2d
Mark Jessop <vk5qi@rfhead.net>
2015-01-24 23:56:15 +1030

    Whoops, bug in openradio_wspr_tx.py

--- openradio_wspr/openradio_wspr_tx.py
+++ openradio_wspr/openradio_wspr_tx.py
@@ -16,9 +16,11 @@ serial_rate = 57600
 
 s = serial.Serial(serial_port, serial_rate, timeout=1)
 
+print "Transmitting every %d minutes." % (minute_mod)
+
 while(True):
 	now = datetime.datetime.now()
-	if(now.second==0 and now.minute%minute_divisor==0):
+	if(now.second==0 and now.minute%minute_mod==0):
 		print "Starting Transmission..."
 		s.write('t')
 		time.sleep(10)
